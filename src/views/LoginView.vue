63b7b9365dd0b17cd11c41d855964b1832f538bc
Jenuel Ganawed <jenuelganawed936@gmail.com>
2023-05-24 18:10:50 +0800

    Update Login Page dark and light thme

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -2,10 +2,12 @@
 import { reactive } from "vue";
 import { Icon } from "@iconify/vue";
 import { useRouter } from "vue-router";
+import useThemeStore from "@/stores/theme";
 
+const themeStore = useThemeStore();
 const router = useRouter();
 const data = reactive({
-    loading: false,
+    loading: false
 });
 
 function submit() {
@@ -18,6 +20,13 @@ function submit() {
 }
 </script>
 <template>
+    <div class="fixed top-10px right-10px text-size-30px cursor-pointer">
+        <transition name="fade">
+            <Icon v-if="themeStore.mode == 'light'" icon="material-symbols:dark-mode-rounded"
+                  @click="themeStore.mode = 'dark'" />
+            <Icon v-else-if="themeStore.mode == 'dark'" icon="ic:sharp-wb-sunny" @click="themeStore.mode = 'light'" />
+        </transition>
+    </div>
     <div class="h-[100vh] flex flex-col">
         <div class="min-h-500px h-full flex items-center justify-center p-20px">
             <div
@@ -26,33 +35,33 @@ function submit() {
                 <div class="max-w-800px w-full">
                     <div>
                         <h1
-                            class="text-size-50px font-800 text-[var(--primary)] mb-5"
+                            class="text-size-50px font-800 text-[var(--primary)] mb-5 select-none"
                         >
                             facebook
                         </h1>
-                        <p class="text-size-28px max-w-700px">
+                        <p class="text-size-28px max-w-700px select-none">
                             Facebook helps you connect and share with the people
                             in your life
                         </p>
                     </div>
                 </div>
                 <div
-                    class="p-5 rounded-lg shadow-lg max-w-400px flex flex-col gap-3 w-full bg-white"
+                    class="p-5 rounded-lg shadow-lg max-w-400px flex flex-col gap-3 w-full light:bg-white dark:bg-[var(--secondary-background)]"
                 >
                     <input
-                        type="text"
+                        class="py-14px px-16px rounded-md w-full outline-none light:border light:active:outline-[var(--primary)] focus:outline-[var(--primary)] dark:bg-[var(--third-background)]"
                         placeholder="Email address or phone number"
-                        class="py-14px px-16px rounded-md w-full border active:outline-[var(--primary)] focus:outline-[var(--primary)]"
+                        type="text"
                     />
                     <input
-                        type="password"
+                        class="py-14px px-16px rounded-md w-full outline-none light:border light:active:outline-[var(--primary)] focus:outline-[var(--primary)] dark:bg-[var(--third-background)]e"
                         placeholder="Password"
-                        class="py-14px px-16px rounded-md w-full border active:outline-[var(--primary)] focus:outline-[var(--primary)]"
+                        type="password"
                     />
                     <button
+                        :disabled="data.loading"
                         class="py-14px px-16px w-full bg-[var(--primary)] text-white text-size-20px font-600 rounded-md"
                         @click="submit"
-                        :disabled="data.loading"
                     >
                         <span v-show="!data.loading">Log In</span>
                         <Icon
@@ -62,26 +71,38 @@ function submit() {
                         />
                     </button>
                     <a
-                        href="#"
-                        class="mx-auto text-[var(--primary)]"
                         :disabled="data.loading"
+                        class="mx-auto text-[var(--primary)]"
+                        href="#"
                     >
                         Forgotten Password
                     </a>
                     <hr />
                     <button
-                        class="py-14px px-16px bg-[var(--green)] text-white text-size-17px rounded-md mx-auto"
                         :disabled="data.loading"
+                        class="py-14px px-16px bg-[var(--green)] text-white text-size-17px rounded-md mx-auto"
                     >
                         Create New Account
                     </button>
                 </div>
             </div>
         </div>
-        <div class="w-full py-5 bg-white">
+        <div class="w-full py-5 light:bg-white dark:bg-[var(--secondary-background)]">
             <div class="max-w-1000px mx-auto w-full">
                 This is a footer, you can add whatever you like in here.
             </div>
         </div>
     </div>
 </template>
+<style>
+.fade-enter-active,
+.fade-leave-active {
+    transition: opacity 0.5s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+    position: absolute;
+    opacity: 0;
+}
+</style>
