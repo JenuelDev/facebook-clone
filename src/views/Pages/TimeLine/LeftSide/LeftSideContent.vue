c584760e83807fc543306f31c9d30dda48f75484
Jenuel Ganawed <jenuelganawed936@gmail.com>
2023-06-04 22:04:20 +0800

    Updates

---
+++ src/views/Pages/TimeLine/LeftSide/LeftSideContent.vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup>
+import { Icon } from "@iconify/vue";
+</script>
+<template>
+    <div>
+        <div>
+            <Icon icon="material-symbols:home" />
+            Home
+        </div>
+        <div></div>
+    </div>
+</template>
--- src/views/Pages/TimeLine/TimeLineView.vue
+++ src/views/Pages/TimeLine/TimeLineView.vue
@@ -1,7 +1,14 @@
 <script lang="ts" setup>
 import HeaderComponent from "./Header/HeaderComponent.vue";
+import LeftSideContent from "./LeftSide/LeftSideContent.vue";
 </script>
 <template>
     <HeaderComponent />
-    <div>Time Line Page</div>
+    <div class="pt-70px flex justify-between">
+        <div>
+            <LeftSideContent />
+        </div>
+        <div>Time Line Page</div>
+        <div></div>
+    </div>
 </template>
