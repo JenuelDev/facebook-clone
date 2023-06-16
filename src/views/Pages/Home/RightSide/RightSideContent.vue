f719844c61193a16d097a1d74c33335527ab3e6e
Jenuel Ganawed <jenuelganawed936@gmail.com>
2023-06-16 20:18:07 +0800

    Updates

--- src/views/Pages/Home/RightSide/RightSideContent.vue
+++ src/views/Pages/Home/RightSide/RightSideContent.vue
@@ -4,7 +4,7 @@ import SponsorsContent from "./Sponsors/SponsorsContent.vue";
 </script>
 <template>
     <div
-        class="w-full max-w-300px pt-10px h-[calc(100% - 70px)] md:block hidden overflow-y-auto overflowing-div pb-20px"
+        class="sticky top-0 w-full max-w-300px pt-10px h-[calc(100% - 70px)] md:block hidden overflow-y-auto overflowing-div pb-20px"
     >
         <SponsorsContent />
         <ContactsContent />
--- src/views/Pages/Home/Screens/TimeLineScreen.vue
+++ src/views/Pages/Home/Screens/TimeLineScreen.vue
@@ -1,8 +1,45 @@
 <template>
-    <div class="px-15px">
+    <div class="px-15px h-full overflow-y-auto pt-2 pb-5">
         this is timeline Lorem ipsum dolor sit, amet consectetur adipisicing
         elit. Repellendus earum maiores dolore ad odio amet officia, quae
         tenetur ipsum natus fuga, aperiam exercitationem pariatur, rem dolor
-        odit itaque officiis laborum.
+        odit itaque officiis laborum. Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Voluptatum quam atque modi. Reiciendis repellendus
+        quos rem ullam laborum odio natus nostrum temporibus assumenda
+        repudiandae impedit libero, enim aspernatur amet vitae! this is timeline
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellendus
+        earum maiores dolore ad odio amet officia, quae tenetur ipsum natus
+        fuga, aperiam exercitationem pariatur, rem dolor odit itaque officiis
+        laborum. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Voluptatum quam atque modi. Reiciendis repellendus quos rem ullam
+        laborum odio natus nostrum temporibus assumenda repudiandae impedit
+        libero, enim aspernatur amet vitae!this is timeline Lorem ipsum dolor
+        sit, amet consectetur adipisicing elit. Repellendus earum maiores dolore
+        ad odio amet officia, quae tenetur ipsum natus fuga, aperiam
+        exercitationem pariatur, rem dolor odit itaque officiis laborum. Lorem
+        ipsum dolor sit amet consectetur adipisicing elit. Voluptatum quam atque
+        modi. Reiciendis repellendus quos rem ullam laborum odio natus nostrum
+        temporibus assumenda repudiandae impedit libero, enim aspernatur amet
+        vitae!this is timeline Lorem ipsum dolor sit, amet consectetur
+        adipisicing elit. Repellendus earum maiores dolore ad odio amet officia,
+        quae tenetur ipsum natus fuga, aperiam exercitationem pariatur, rem
+        dolor odit itaque officiis laborum. Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Voluptatum quam atque modi. Reiciendis
+        repellendus quos rem ullam laborum odio natus nostrum temporibus
+        assumenda repudiandae impedit libero, enim aspernatur amet vitae!this is
+        timeline Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+        Repellendus earum maiores dolore ad odio amet officia, quae tenetur
+        ipsum natus fuga, aperiam exercitationem pariatur, rem dolor odit itaque
+        officiis laborum. Lorem ipsum dolor sit amet consectetur adipisicing
+        elit. Voluptatum quam atque modi. Reiciendis repellendus quos rem ullam
+        laborum odio natus nostrum temporibus assumenda repudiandae impedit
+        libero, enim aspernatur amet vitae!this is timeline Lorem ipsum dolor
+        sit, amet consectetur adipisicing elit. Repellendus earum maiores dolore
+        ad odio amet officia, quae tenetur ipsum natus fuga, aperiam
+        exercitationem pariatur, rem dolor odit itaque officiis laborum. Lorem
+        ipsum dolor sit amet consectetur adipisicing elit. Voluptatum quam atque
+        modi. Reiciendis repellendus quos rem ullam laborum odio natus nostrum
+        temporibus assumenda repudiandae impedit libero, enim aspernatur amet
+        vitae!
     </div>
 </template>
