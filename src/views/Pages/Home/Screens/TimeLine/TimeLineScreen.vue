ba03690fd1a40aaff0a1c8ad3ea625a085a0b2cb
Jenuel Ganawed <jenuelganawed936@gmail.com>
2023-06-20 17:37:00 +0800

    update

--- src/views/Pages/Home/Screens/TimeLine/TimeLineScreen.vue
+++ src/views/Pages/Home/Screens/TimeLine/TimeLineScreen.vue
@@ -1,51 +1,12 @@
 <script setup lang="ts">
 import StoriesTimeLine from "./Stories/StoriesTimeLine.vue";
+import FeedTimeline from "./Feed/FeedTimeline.vue";
 </script>
 <template>
     <div class="px-15px h-full overflow-y-auto pt-2 pb-5">
         <div>
             <StoriesTimeLine />
         </div>
-        this is timeline Lorem ipsum dolor sit, amet consectetur adipisicing
-        elit. Repellendus earum maiores dolore ad odio amet officia, quae
-        tenetur ipsum natus fuga, aperiam exercitationem pariatur, rem dolor
-        odit itaque officiis laborum. Lorem ipsum dolor sit amet consectetur
-        adipisicing elit. Voluptatum quam atque modi. Reiciendis repellendus
-        quos rem ullam laborum odio natus nostrum temporibus assumenda
-        repudiandae impedit libero, enim aspernatur amet vitae! this is timeline
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellendus
-        earum maiores dolore ad odio amet officia, quae tenetur ipsum natus
-        fuga, aperiam exercitationem pariatur, rem dolor odit itaque officiis
-        laborum. Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Voluptatum quam atque modi. Reiciendis repellendus quos rem ullam
-        laborum odio natus nostrum temporibus assumenda repudiandae impedit
-        libero, enim aspernatur amet vitae!this is timeline Lorem ipsum dolor
-        sit, amet consectetur adipisicing elit. Repellendus earum maiores dolore
-        ad odio amet officia, quae tenetur ipsum natus fuga, aperiam
-        exercitationem pariatur, rem dolor odit itaque officiis laborum. Lorem
-        ipsum dolor sit amet consectetur adipisicing elit. Voluptatum quam atque
-        modi. Reiciendis repellendus quos rem ullam laborum odio natus nostrum
-        temporibus assumenda repudiandae impedit libero, enim aspernatur amet
-        vitae!this is timeline Lorem ipsum dolor sit, amet consectetur
-        adipisicing elit. Repellendus earum maiores dolore ad odio amet officia,
-        quae tenetur ipsum natus fuga, aperiam exercitationem pariatur, rem
-        dolor odit itaque officiis laborum. Lorem ipsum dolor sit amet
-        consectetur adipisicing elit. Voluptatum quam atque modi. Reiciendis
-        repellendus quos rem ullam laborum odio natus nostrum temporibus
-        assumenda repudiandae impedit libero, enim aspernatur amet vitae!this is
-        timeline Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-        Repellendus earum maiores dolore ad odio amet officia, quae tenetur
-        ipsum natus fuga, aperiam exercitationem pariatur, rem dolor odit itaque
-        officiis laborum. Lorem ipsum dolor sit amet consectetur adipisicing
-        elit. Voluptatum quam atque modi. Reiciendis repellendus quos rem ullam
-        laborum odio natus nostrum temporibus assumenda repudiandae impedit
-        libero, enim aspernatur amet vitae!this is timeline Lorem ipsum dolor
-        sit, amet consectetur adipisicing elit. Repellendus earum maiores dolore
-        ad odio amet officia, quae tenetur ipsum natus fuga, aperiam
-        exercitationem pariatur, rem dolor odit itaque officiis laborum. Lorem
-        ipsum dolor sit amet consectetur adipisicing elit. Voluptatum quam atque
-        modi. Reiciendis repellendus quos rem ullam laborum odio natus nostrum
-        temporibus assumenda repudiandae impedit libero, enim aspernatur amet
-        vitae!
+        <FeedTimeline />
     </div>
 </template>
